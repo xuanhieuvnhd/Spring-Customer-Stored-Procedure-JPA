175fc9920ba8b284d3b69df3d4dfb0d94ce392a0
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-10-21 15:28:08 +0700

    update

---
+++ src/main/java/com/model/data.sql
@@ -0,0 +1,8 @@
+create database customers;
+use customers;
+DELIMITER //
+CREATE PROCEDURE Insert_Customer(IN first_name VARCHAR(255), IN last_name VARCHAR(255))
+BEGIN
+    INSERT INTO customers(firstName,lastName) VALUES (first_name,last_name);
+END//
+DELIMITER ;
